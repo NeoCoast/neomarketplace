9f51b8e862c02642c61ad95b31e1caee7e971d88
Gonzalo Sismondi <45953197+gonzasismo@users.noreply.github.com>
2023-01-24 12:00:35 -0300

    [0th/Product_List] - Add product items list (#3)
    * [0th/Product_List] - Add Product item List
    
    * [0th/Product_list] - Add feedback changes

---
+++ packages/react-app/src/components/ProductItem/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { format }  from 'date-fns';
+
+import MessagesIcon from 'assets/Msgs.svg';
+import DefaultUser from 'assets/DefaultUser.png';
+
+import './styles.scss';
+
+type ProductItemProps = {
+  name: string,
+  image: string,
+  msgsCount: number,
+  price: number,
+  publicationDate: string,
+  seller: string,
+}
+
+const ProductItem = ({
+  name,
+  image,
+  msgsCount,
+  price,
+  publicationDate,
+  seller,
+} : ProductItemProps) => (
+  <div className="product-item__card">
+    <img
+      className="product-item__img"
+      alt="product img"
+      src={`data:image/jpeg;base64,${image}`}
+    />
+    <div className="product-item__body">
+      <span className="product-item__name">{name}</span>
+      <div className="product-item__price-container">
+        <span className="product-item__price">
+          $ {price}
+        </span>
+        <span className="product-item__date">{format(new Date(publicationDate), 'MM/dd/yyyy')}</span>
+      </div>
+      <div className="product-item__divider" />
+      <div className="product-item__seller-container">
+        <div className="product-item__avatar-container">
+          <img src={DefaultUser} alt="User Avatar" className="product-item__avatar" />
+          <span className="product-item__seller-name">{seller}</span>
+        </div>
+        <div className="product-item__msgs-container">
+          <img
+            src={MessagesIcon}
+            alt="Messages inbox"
+          />
+          <span className="product-item__msgs-count">{msgsCount}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default ProductItem;
---
+++ packages/react-app/src/components/ProductList/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import ProductItem from '../ProductItem';
+
+import './styles.scss';
+
+interface Product {
+  id: number,
+  name: string,
+  image: string,
+  msgsCount: number,
+  price: number,
+  publicationDate: string,
+  seller: string,
+}
+
+type ProductListProps = {
+  products?: Product[],
+}
+
+const ProductList = ({ products = [] } : ProductListProps) => (
+  <div className="product-list">
+    {
+      products.map((product) => (
+        <ProductItem {...product} key={product.id} />
+      ))
+    }
+  </div>
+);
+
+export default ProductList;
