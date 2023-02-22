5052ba850d0a983f286b14f54858698c4c7c0804
Juan Manuel <juanmanuel.fernandez@neocoast.com>
2023-02-22 15:12:55 -0300

    WIP - trpc config

--- packages/react-app/src/router.tsx
+++ packages/react-app/src/router.tsx
@@ -8,6 +8,8 @@ import {
   Route,
   BrowserRouter,
 } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { httpBatchLink } from '@trpc/client';
 
 import { UserType } from 'types/user';
 import { users } from 'data/mockedData';
@@ -24,6 +26,7 @@ import NewProduct from 'containers/NewProduct';
 import ItemView from 'containers/ItemView';
 import NotFound from 'containers/NotFound';
 import EditProduct from 'containers/EditProduct';
+import trpc from './utils/trpc';
 
 import './index.scss';
 
@@ -34,6 +37,14 @@ const Router = () => {
     image: '',
   } as UserType);
   const [usersList, setUsersList] = useState<UserType[]>([]);
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() => trpc.createClient({
+    links: [
+      httpBatchLink({
+        url: 'http://localhost:3001/trpc',
+      }),
+    ],
+  }));
 
   const contextValue = useMemo(
     () => ({ selectedUser, setSelectedUser, usersList }),
@@ -47,23 +58,27 @@ const Router = () => {
   }, []);
 
   return (
-    <BrowserRouter>
-      <UserContext.Provider value={contextValue}>
-        <Routes>
-          <Route path={routes.home} element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path={routes.item} element={<ItemView />} />
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <UserContext.Provider value={contextValue}>
+            <Routes>
+              <Route path={routes.home} element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path={routes.item} element={<ItemView />} />
 
-            <Route path="*" element={<NotFound />} />
-            {/* TODO: mejorar not found screen */}
-            <Route path={routes.myPurchased} element={<MyPurchased />} />
-            <Route path={routes.myListing} element={<MyListing />} />
-            <Route path={routes.newProduct} index element={<NewProduct />} />
-            <Route path={routes.editProduct} index element={<EditProduct />} />
-          </Route>
-        </Routes>
-      </UserContext.Provider>
-    </BrowserRouter>
+                <Route path="*" element={<NotFound />} />
+                {/* TODO: mejorar not found screen */}
+                <Route path={routes.myPurchased} element={<MyPurchased />} />
+                <Route path={routes.myListing} element={<MyListing />} />
+                <Route path={routes.newProduct} index element={<NewProduct />} />
+                <Route path={routes.editProduct} index element={<EditProduct />} />
+              </Route>
+            </Routes>
+          </UserContext.Provider>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </trpc.Provider>
   );
 };
 
