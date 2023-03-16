c88b788be5c83c91ea67bbd732e09a7a8ca84054
Juan Manuel <juanmanuel.fernandez@neocoast.com>
2023-03-16 13:29:30 -0300

    fix users  query

---
+++ packages/react-app/src/containers/App/index.tsx
@@ -0,0 +1,28 @@
+import React, { useState, ReactNode } from 'react';
+import { httpBatchLink } from '@trpc/client';
+
+import trpc from 'utils/trpc';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+type AppProps = { children : ReactNode }
+
+const App = ({ children } : AppProps) => {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() => trpc.createClient({
+    links: [
+      httpBatchLink({
+        url: 'http://localhost:3001/trpc',
+      }),
+    ],
+  }));
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+};
+
+export default App;
--- packages/react-app/src/router.tsx
+++ packages/react-app/src/router.tsx
@@ -1,23 +1,19 @@
 import React, {
-  useEffect,
   useMemo,
   useState,
-  useCallback,
 } from 'react';
 import {
   Routes,
   Route,
   BrowserRouter,
 } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { httpBatchLink, createTRPCProxyClient } from '@trpc/client';
+import { ClipLoader } from 'react-spinners';
 
 import { UserType } from 'types/user';
 
 import UserContext from 'context';
 
 import routes from 'constants/routes';
-import type { AppRouter } from '@server/routers/app';
 
 import Home from 'containers/Home';
 import MyPurchased from 'containers/MyPurchased';
@@ -27,9 +23,9 @@ import NewProduct from 'containers/NewProduct';
 import ItemView from 'containers/ItemView';
 import NotFound from 'containers/NotFound';
 import EditProduct from 'containers/EditProduct';
-import trpc from 'utils/trpc';
 
 import './index.scss';
+import trpc from 'utils/trpc';
 
 const Router = () => {
   const [selectedUser, setSelectedUser] = useState({
@@ -37,62 +33,58 @@ const Router = () => {
     name: '',
     image: '',
   } as UserType);
-  const [usersList, setUsersList] = useState<UserType[]>([]);
-  const [queryClient] = useState(() => new QueryClient());
-  const [trpcClient] = useState(() => trpc.createClient({
-    links: [
-      httpBatchLink({
-        url: 'http://localhost:3001/trpc',
-      }),
-    ],
-  }));
 
-  const client = createTRPCProxyClient<AppRouter>(
-    {
-      links: [
-        httpBatchLink({ url: 'http://localhost:3001/trpc' }),
-      ],
-    },
-  );
+  const usersData = trpc.user.getAll.useQuery();
 
   const contextValue = useMemo(
-    () => ({ selectedUser, setSelectedUser, usersList }),
-    [selectedUser, usersList],
+    () => ({ selectedUser, setSelectedUser, usersList: usersData.isSuccess ? usersData.data : [] }),
+    [selectedUser, usersData.isSuccess],
   );
 
-  const getUsersCall = useCallback(async () => {
-    const users = await client.user.getAll.query();
+  if (usersData.isLoading) {
+    return (
+      <ClipLoader
+        className="App__loader"
+        size={70}
+        loading={usersData.isLoading}
+        color="#2C3A61"
+      />
+    );
+  }
 
-    setSelectedUser(users?.[1]);
-    setUsersList(users);
-  }, []);
+  if (usersData.isError) {
+    // TODO: mejorar error screen
+    return (
+      <div>
+        Error loading users.
+      </div>
+    );
+  }
 
-  useEffect(() => {
-    getUsersCall();
-  }, [getUsersCall]);
+  if (usersData.isSuccess && !selectedUser.id) {
+    const user = usersData.data[0];
+
+    setSelectedUser(user);
+  }
 
   return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <UserContext.Provider value={contextValue}>
-            <Routes>
-              <Route path={routes.home} element={<Layout />}>
-                <Route index element={<Home />} />
-                <Route path={routes.item} element={<ItemView />} />
+    <BrowserRouter>
+      <UserContext.Provider value={contextValue}>
+        <Routes>
+          <Route path={routes.home} element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path={routes.item} element={<ItemView />} />
 
-                <Route path="*" element={<NotFound />} />
-                {/* TODO: mejorar not found screen */}
-                <Route path={routes.myPurchased} element={<MyPurchased />} />
-                <Route path={routes.myListing} element={<MyListing />} />
-                <Route path={routes.newProduct} index element={<NewProduct />} />
-                <Route path={routes.editProduct} index element={<EditProduct />} />
-              </Route>
-            </Routes>
-          </UserContext.Provider>
-        </BrowserRouter>
-      </QueryClientProvider>
-    </trpc.Provider>
+            <Route path="*" element={<NotFound />} />
+            {/* TODO: mejorar not found screen */}
+            <Route path={routes.myPurchased} element={<MyPurchased />} />
+            <Route path={routes.myListing} element={<MyListing />} />
+            <Route path={routes.newProduct} index element={<NewProduct />} />
+            <Route path={routes.editProduct} index element={<EditProduct />} />
+          </Route>
+        </Routes>
+      </UserContext.Provider>
+    </BrowserRouter>
   );
 };
 
