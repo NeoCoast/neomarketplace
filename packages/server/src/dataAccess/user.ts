cc2cdba4bf71455ecf81a48b9025cff284a95ea3
liaMalvarez <lia@neocoast.com>
2023-03-21 15:27:05 -0300

    reestructures the repo with new names for frontend and backend folders

--- packages/server/src/dataAccess/user.ts
+++ packages/server/src/dataAccess/user.ts
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient, User } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -10,4 +10,4 @@ export const getById = async (id: number) => {
   return user;
 };
 
-export const getAll = async () => await prisma.user.findMany();
+export const getAll = () => prisma.user.findMany();
